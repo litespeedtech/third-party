cafd638f8bc3e8a720312b7e0641e9a2fe22ff44
George Wang <gwang@litespeedtech.com>
2019-10-11 14:03:02 -0400

    create lib64 dirctory if not exists.

--- script/build_lslb.sh
+++ script/build_lslb.sh
@@ -4,6 +4,10 @@ BUILD_LIBS="brotli zlib bssl expat geoip ip2loc libmaxminddb luajit pcre psol ud
 cd `dirname "$0"`
 cd ..
 
+if [ ! -d lib64 ]; then
+    mkdir lib64
+fi
+
 git submodule update --init
 cd script
 
