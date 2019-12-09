c6277fe66f9b6f5a0f59923af1ca291b379ffac5
raihan001 <53560730+raihan001@users.noreply.github.com>
2019-12-09 18:59:02 +0700

    Update build_luajit.sh

--- script/build_luajit.sh
+++ script/build_luajit.sh
@@ -5,8 +5,10 @@ cd ..
 CUR_PREFIX=`pwd`
 cd src
 
+rm -rf luajit
+git clone https://github.com/cbaylis/luajit-aarch64.git
+mv luajit-aarch64 luajit
 cd luajit
-
 make -j4 EXTRA_CMAKE_OPTIONS='-DCMAKE_C_COMPILER=arm64-linux-gcc -DCMAKE_CXX_COMPILER=arm64-linux-gnu-g++ -DCXX_STANDARD_REQUIRED=c++17' install PREFIX=$CUR_PREFIX CFLAGS="-fPIC -O3" 
 
 cd ../..
