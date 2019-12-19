1d2d84f6e9069187220b125790587d62daedc0ef
George Wang <gwang@litespeedtech.com>
2019-12-19 18:53:33 -0500

    Update build_pcre.sh, enable UTF-8 and Unicode properties support.

--- script/build_pcre.sh
+++ script/build_pcre.sh
@@ -13,6 +13,6 @@ tar xvfz $PACKAGE.tar.gz
 rm $PACKAGE.tar.gz
 
 cd $PACKAGE
-./configure --prefix=$PREFIX --enable-jit --with-pic --with-match-limit=100000 --enable-shared=no
+./configure --prefix=$PREFIX --enable-jit --enable-unicode-properties --enable-utf --with-pic --with-match-limit=100000 --enable-shared=no
 make install
 
