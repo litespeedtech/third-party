686ccce322e9c6eb04bd7e52cf95338bba484a95
George Wang <gwang@litespeedtech.com>
2020-04-15 18:28:59 -0400

    Update expact build script to use cmake.

--- script/build_expat.sh
+++ script/build_expat.sh
@@ -9,10 +9,16 @@ git clone https://github.com/libexpat/libexpat.git
 
 cd libexpat
 
-git checkout R_2_2_6
+git checkout R_2_2_9
 cd expat
 
-./buildconf.sh
-./configure --prefix=$PREFIX --without-xmlwf --without-docbook --enable-shared=no
+rm -rf build
+
+mkdir build
+cd build 
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$PREFIX -DEXPAT_SHARED_LIBS=OFF ..
+
+#./buildconf.sh
+#./configure --prefix=$PREFIX --without-xmlwf --without-docbook --enable-shared=no
 make install
 
