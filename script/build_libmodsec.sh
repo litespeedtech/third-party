8da74e453ff1addb35bd2528a6ba43afa5d9e9bf
George Wang <gwang@litespeedtech.com>
2020-06-16 13:03:29 -0400

    use build_yajl.sh

--- script/build_libmodsec.sh
+++ script/build_libmodsec.sh
@@ -6,25 +6,7 @@ cd src
 
 
 if [ ! -d lloyd-yajl-66cb08c ]; then
-    # Install YAJL2
-    wget http://github.com/lloyd/yajl/tarball/2.1.0 -O yajl-2.1.0.tar.gz
-    tar -xvzf yajl-2.1.0.tar.gz
-    cd lloyd-yajl-66cb08c
-    ./configure -p $PREFIX
-
-    cd build
-    cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_C_FLAGS="-fPIC" ..
-    cd ..
-
-    make install
-    cd ../..
-    cp patches/libtool/libyajl.la lib/libyajl.la
-    echo "libdir='$PREFIX/lib'" >> lib/libyajl.la
-    mv lib/libyajl_s.a lib/libyajl.a
-    rm -f lib/libyajl.so*
-
-    #yum install -y doxygen  flex
-    #./build_autotool.sh
+    ../script/build_yajl.sh
     cd src
 fi
 
