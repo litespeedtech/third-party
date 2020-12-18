638388bff30c3aff5eef5314a76884cac7b15ee8
George Wang <gwang@litespeedtech.com>
2020-12-18 10:15:21 -0500

    Update zmq to release v4.3.3, switch to DEBUG build.

--- script/build_zmq.sh
+++ script/build_zmq.sh
@@ -11,10 +11,15 @@ cd src
 if [ ! -d libzmq ]; then
     git clone https://github.com/zeromq/libzmq.git
 fi
-    cd libzmq/
+    cd libzmq 
+
+    git reset --hard
+    git pull
+    git checkout v4.3.3
+
     mkdir build
     cd build
-    cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=RelWithDbgInfo -DWITH_PERF_TOOL=OFF -DZMQ_BUILD_TESTS=OFF ..
+    cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=Debug -DWITH_PERF_TOOL=OFF -DZMQ_BUILD_TESTS=OFF ..
     make 
 #    ./autogen.sh 
 #    ./configure --prefix=$PREFIX --enable-shared=no
