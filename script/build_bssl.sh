49f7609bef74b24c8a7312e4bbbd261bbed5d74c
George Wang <gwang@litespeedtech.com>
2021-02-10 15:02:23 -0500

    Update boringSSL with latest HTTP3 support.

--- script/build_bssl.sh
+++ script/build_bssl.sh
@@ -24,7 +24,9 @@ git pull
 #git checkout 49de1fc291
 #git checkout bfe527fa35735e8e045cbfb42b012e13ca68f9cf
 #git checkout 251b5169fd44345f455438312ec4e18ae07fd58c
-git checkout b117a3a0b7bd11fe6ebd503ec6b45d6b910b41a1
+#git checkout b117a3a0b7bd11fe6ebd503ec6b45d6b910b41a1
+# HTTP/3 v1 and ID-34 support
+git checkout a2278d4d2cabe73f6663e3299ea7808edfa306b9
 
 rm -rf build
 
