2bfb25d50b9747eac45dc99376515713199e24ca
George Wang <gwang@litespeedtech.com>
2022-08-08 17:51:32 -0400

    add build_lsphp.sh

---
+++ script/build_lsphp.sh
@@ -0,0 +1,19 @@
+
+cd `dirname "$0"`
+cd ..
+PREFIX=`pwd`
+cd src
+
+PACKAGE=php-7.4.30
+
+if [ ! -d $PACKAGE ]; then
+    wget https://www.php.net/distributions/$PACKAGE.tar.gz
+fi
+tar xvfz $PACKAGE.tar.gz
+rm $PACKAGE.tar.gz
+
+cd $PACKAGE
+./configure PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ --prefix=$PREFIX --disable-all --enable-litespeed --enable-session --enable-posix --enable-xml --with-libexpat-dir=$PREFIX --with-zlib=$PREFIX --enable-sockets --enable-bcmath --enable-json --with-libxml=$PREFIX
+make -j4
+cp sapi/litespeed/php ../../bin/lsphp
+
