effa290874d0432c49cf356e63726fd065b7c102
George Wang <gwang@litespeedtech.com>
2023-07-20 08:45:14 -0400

    update libcurl to 7.88.1

--- script/build_curl.sh
+++ script/build_curl.sh
@@ -11,7 +11,8 @@ if [ ! -d "curl" ] ; then
 fi
 
     cd curl
-    git checkout curl-7_65_3
+    git pull
+    git checkout curl-7_88_1
     ./buildconf 
     CFLAGS="-fPIC" LIBS=-lpthread ./configure --prefix=$PREFIX --with-ssl=$PREFIX --with-brotli=no  --enable-shared=no --enable-ldap=no --without-nghttp2 --without-nghttp3
     make
