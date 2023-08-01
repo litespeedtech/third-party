c4f1e4bec0d48d874ee0f241f3c02206d9c93725
George Wang <gwang@litespeedtech.com>
2023-07-31 22:44:12 -0400

    Update libmodsec to 3.0.10

--- script/build_libmodsec.sh
+++ script/build_libmodsec.sh
@@ -18,7 +18,7 @@ cd ModSecurity
 git reset --hard
 git checkout -b v3/master origin/v3/master
 git pull
-git checkout v3.0.9
+git checkout v3.0.10
 
 export PATH=/home/build/tools/bin/:$PATH
 sh build.sh
