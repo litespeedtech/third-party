7d346aa29de706d9990131047604315f40bf37cd
Robert Perper <rperper@litespeedtech.com>
2023-08-28 15:07:43 -0400

    Fixed libinjection to use python2 when necessary and added ccronexpr and re2 to build_all

--- script/build_libinjection.sh
+++ script/build_libinjection.sh
@@ -1,4 +1,4 @@
-
+#!/bin/sh
 cd `dirname "$0"`
 cd ..
 PREFIX=`pwd`
@@ -7,7 +7,15 @@ PREFIX=`pwd`
 cd src/libinjection
 git reset --hard
 git checkout v3.10.0
+cd src
 
+PYFULLVER=$(/usr/bin/env python --version)
+PYVER=$(echo "$PYFULLVER" | cut -c 8)
+if [ "$PYVER" = "3" ]; then
+   echo "Change python scripts to use Python2"
+   sed -i 's~/usr/bin/env python~/usr/bin/env python2~' *.py
+fi
+cd ..
 make
 cp src/*.h ../../include
 cp src/libinjection.a ../../lib
