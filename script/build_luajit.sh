aeccb1cf4287d203f8b542f5b0dfb7e3dc597c95
George Wang <gwang@litespeedtech.com>
2023-09-07 19:24:19 -0400

    Update libluajit to 2.1 , create lib/libluajit.a symbolic link.

--- script/build_luajit.sh
+++ script/build_luajit.sh
@@ -12,6 +12,7 @@ make install PREFIX=$CUR_PREFIX CFLAGS="-fPIC -O3"
 cd ../..
 cp patches/libtool/libluajit-5.1.la lib/libluajit-5.1.la
 echo "libdir='$CUR_PREFIX/lib'" >> lib/libluajit-5.1.la
+ln -sf libluajit-5.1.a lib/libluajit.a
 
-rm -f lib/libluajit*.so.*
+rm -f lib/libluajit*.so*
 
