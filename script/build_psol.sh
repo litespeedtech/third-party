ff779f831b010505bdfcb73790a4457904a0112e
George Wang <gwang@litespeedtech.com>
2024-10-16 15:05:30 -0400

    fix bad scoped_ptr.h header file for gcc version >= 8.

--- script/build_psol.sh
+++ script/build_psol.sh
@@ -23,5 +23,25 @@ if [ ! -f psol-$PSOL_VER/include/out/Release/obj/gen/net/instaweb/public/version
     mv psol psol-$PSOL_VER
     cd ..
     ln -sf src/psol-$PSOL_VER psol-$PSOL_VER
+
+    cat << EOF > psol-$PSOL_VER/include/pagespeed/kernel/base/scoped_ptr.h
+/**
+* Due the compiling issue, this file was updated from the original file.
+*/
+#ifndef PAGESPEED_KERNEL_BASE_SCOPED_PTR_H_
+#define PAGESPEED_KERNEL_BASE_SCOPED_PTR_H_
+#include "base/memory/scoped_ptr.h"
+
+namespace net_instaweb {
+template<typename T> class scoped_array : public scoped_ptr<T[]> {
+public:
+    scoped_array() : scoped_ptr<T[]>() {}
+    explicit scoped_array(T* t) : scoped_ptr<T[]>(t) {}
+};
+}
+#endif
+
+EOF    
+
 fi
 
