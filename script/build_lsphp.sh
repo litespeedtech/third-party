1b0335563a0eff538a0aca3b94932f0dc3ff930d
George Wang <gwang@litespeedtech.com>
2026-04-07 20:54:22 -0400

    update build_lsphp.sh, download the latest version of php-litespeed sapi.

--- script/build_lsphp.sh
+++ script/build_lsphp.sh
@@ -10,14 +10,37 @@ else
     PACKAGE=php-8.5.2
 fi
 
+LSAPI_VERSION=8.3
+
 if [ ! -d $PACKAGE ]; then
     wget https://www.php.net/distributions/$PACKAGE.tar.gz
+    tar xvfz $PACKAGE.tar.gz
+    rm $PACKAGE.tar.gz
 fi
-tar xvfz $PACKAGE.tar.gz
-rm $PACKAGE.tar.gz
 
 cd $PACKAGE
-./configure PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ --prefix=$PREFIX --disable-all --enable-litespeed --enable-session --enable-posix --enable-xml --with-libexpat-dir=$PREFIX --with-zlib=$PREFIX --enable-sockets --enable-bcmath --enable-json --with-libxml=$PREFIX
+
+if [ ! -f php-litespeed-${LSAPI_VERSION}.tgz ]; then
+    wget https://www.litespeedtech.com/packages/lsapi/php-litespeed-${LSAPI_VERSION}.tgz
+fi 
+
+cd sapi
+if [ -e litespeed/Makefile.frag ] ; then
+        mv -f litespeed/Makefile.frag litespeed/Makefile.frag.package
+fi
+
+tar -xzf "../php-litespeed-${LSAPI_VERSION}.tgz" -C litespeed --strip-components=1
+
+if [ -e litespeed/Makefile.frag.package ] ; then
+        mv -f litespeed/Makefile.frag.package litespeed/Makefile.frag
+fi
+cd ..
+
+./configure PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ --prefix=$PREFIX --disable-all \
+	--with-litespeed --enable-litespeed --enable-session --enable-posix \
+	--enable-xml --with-libexpat-dir=$PREFIX --with-zlib=$PREFIX \
+	--enable-sockets --enable-bcmath --enable-json \
+	--with-libxml=$PREFIX --enable-hash
 make -j4
 if [ -f sapi/litespeed/lsphp ]; then
     cp sapi/litespeed/lsphp ../../bin/lsphp
