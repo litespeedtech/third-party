f884eaf2cbc2b6cc1a0009b8eae1558ef0bc1593
George Wang <gwang@litespeedtech.com>
2026-04-23 09:37:21 -0400

    update libxml2 to v2.15.3 .

--- script/build_libxml2.sh
+++ script/build_libxml2.sh
@@ -4,9 +4,9 @@ cd `dirname "$0"`
 cd ..
 PREFIX=`pwd`
 cd src
-VERSION=2.14.2
+VERSION=2.15.3
 if [ ! -e "libxml2-$VERSION" ] ; then
-    wget https://download.gnome.org/sources/libxml2/2.14/libxml2-$VERSION.tar.xz
+    wget https://download.gnome.org/sources/libxml2/2.15/libxml2-$VERSION.tar.xz
     tar xvfJ libxml2-$VERSION.tar.xz
     ln -sf libxml2-$VERSION libxml2
 fi
